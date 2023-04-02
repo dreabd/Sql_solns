c05ca33321596264469b0573d4b09ad7b6815a17
Andre <andreabad17@gmail.com>
2023-04-01 21:30:40 -0700

    Subqueries for Days

--- study_session/solutions.sql
+++ study_session/solutions.sql
@@ -10,15 +10,15 @@ WHERE email LIKE "%gmail%";
 -- Jane Doe, Minnie Mouse, Mark Doe
 
 -- Teacher who make > 50,000 and is Married
-SELECT * 
+SELECT *
 FROM teachers
 WHERE salary > 50000 AND marriage_status = 1
 LIMIT 1;
 -- Should return Mickey Mouse
 
--- Return name of Teachers who own Duct Tape 
+-- Return name of Teachers who own Duct Tape
 SELECT teachers.first_name, teachers.last_name
-FROM teachers 
+FROM teachers
 JOIN teacher_supplies ON (teachers.id = teacher_supplies.teacher_id)
 JOIN supplies ON (supplies.id = teacher_supplies.supply_id)
 WHERE supplies.name = "Duct Tape";
@@ -27,13 +27,13 @@ WHERE supplies.name = "Duct Tape";
 -- Get rid of the whiteboard markers and tissues of teachers who make more than 50000
 SELECT * from teacher_supplies;
 
-UPDATE teacher_supplies
-SET teacher_supplies.quantity = 0
+.
+
+SELECT teachers.first_name, teachers.last_name, teachers.salary, supplies.name
+FROM teacher_supplies
 JOIN teachers ON (teachers.id = teacher_supplies.teacher_id)
 JOIN supplies ON (supplies.id = teacher_supplies.supply_id)
-WHERE teachers.salary > 50000 AND (supplies.name = "Whiteboard Markers" OR supplies.name = "Tissues");
-
-SELECT * from teacher_supplies;
+WHERE supplies.name = "Whiteboard Markers" OR supplies.name = "Tissues";
 -- This one is a struggle
 
 -- Update Mickey and Minnie's marraige status to false
@@ -43,7 +43,7 @@ WHERE teachers.last_name = "Mouse";
 
 
 -- Delete Mickey from the system
-DELETE FROM teachers 
+DELETE FROM teachers
 WHERE first_name = "Mickey" AND last_name = "Mouse";
 
 -- Update Minnie's Number 9999999999
@@ -55,6 +55,3 @@ WHERE teachers.last_name = "Mouse";
 UPDATE teachers
 SET last_name = "Mouse", marriage_status = 1
 WHERE teachers.first_name = "Jane" AND teachers.last_name = "Doe";
-
-
-
